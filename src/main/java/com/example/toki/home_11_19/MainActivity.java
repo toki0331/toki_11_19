a08359792b1b4b6f5d4e5ebe5c3b1a3654699689
古川時 <toki@furukawaji-no-MacBook-Air.local>
2017-11-20 00:09:02 +0530

    home_11_19

---
+++ src/main/java/com/example/toki/home_11_19/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.toki.home_11_19;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(this);
+    }
+    public void onClick(View view)
+    {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+}
+
